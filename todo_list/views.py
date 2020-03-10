d8c223f09a5876f70f0156fc3dc191b2abf1c4fd
fares siouane <fares.geek.dz@gmail.com>
2020-03-10 21:51:35 +0100

    setup create task functionallity

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
+from .forms import ListForm
 
 def home(request):
-    tasks = List.objects.all()
-    return render(request, 'home.html', {'tasks' : tasks})
+    if request.method == "POST":
+        form = ListForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+
+    else:
+        tasks = List.objects.all()
+        return render(request, 'home.html', {'tasks' : tasks,})
