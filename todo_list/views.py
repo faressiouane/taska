c4e58be75b4fe573032b81e089bb8b85d25143eb
fares siouane <fares.geek.dz@gmail.com>
2020-03-10 22:07:04 +0100

    fix

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
+from django.contrib import messages
 
 def home(request):
     if request.method == "POST":
         form = ListForm(request.POST or None)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Task has been added to the list')
         return redirect('home')
 
     else:
